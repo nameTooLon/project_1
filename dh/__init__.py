2395b9942bc714b604197c74123fc2087e17c866
--global <--global>
2014-04-28 04:48:07 +1000

    ok, so I am getting somewhere but slow. Need to hash message into 32 bits in order to send using AES

--- dh/__init__.py
+++ dh/__init__.py
@@ -1,46 +1,88 @@
-from Crypto.Hash import SHA
+from Crypto.Hash import SHA256
+from Crypto.Hash import HMAC
 from Crypto.Random import random
+
+from lib.helpers import read_hex
 
 # Project TODO: Is this the best choice of prime? Why? Why not? Feel free to replace!
 
-# 1536 bit safe prime for Diffie-Hellman key exchange
+# PEZZ -- Changed the prime to 2048-bit MODP group (to be sure the exponent size is
+# not the weakest point) (Has double the entropy of the strength of the entire system)
+# 2048 bit safe prime for Diffie-Hellman key exchange
 # obtained from RFC 3526
-prime = int("0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1\
-29024E088A67CC74020BBEA63B139B22514A08798E3404DD\
-EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245\
-E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED\
-EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D\
-C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F\
-83655D23DCA3AD961C62F356208552BB9ED529077096966D\
-670C354E4ABC9804F1746C08CA237327FFFFFFFFFFFFFFFF", 16)
+raw_prime_1536 = """FFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1
+      29024E08 8A67CC74 020BBEA6 3B139B22 514A0879 8E3404DD
+      EF9519B3 CD3A431B 302B0A6D F25F1437 4FE1356D 6D51C245
+      E485B576 625E7EC6 F44C42E9 A637ED6B 0BFF5CB6 F406B7ED
+      EE386BFB 5A899FA5 AE9F2411 7C4B1FE6 49286651 ECE45B3D
+      C2007CB8 A163BF05 98DA4836 1C55D39A 69163FA8 FD24CF5F
+      83655D23 DCA3AD96 1C62F356 208552BB 9ED52907 7096966D
+      670C354E 4ABC9804 F1746C08 CA237327 FFFFFFFF FFFFFFFF"""
 
-print(prime)
+raw_prime_2048 = """FFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1
+      29024E08 8A67CC74 020BBEA6 3B139B22 514A0879 8E3404DD
+      EF9519B3 CD3A431B 302B0A6D F25F1437 4FE1356D 6D51C245
+      E485B576 625E7EC6 F44C42E9 A637ED6B 0BFF5CB6 F406B7ED
+      EE386BFB 5A899FA5 AE9F2411 7C4B1FE6 49286651 ECE45B3D
+      C2007CB8 A163BF05 98DA4836 1C55D39A 69163FA8 FD24CF5F
+      83655D23 DCA3AD96 1C62F356 208552BB 9ED52907 7096966D
+      670C354E 4ABC9804 F1746C08 CA18217C 32905E46 2E36CE3B
+      E39E772C 180E8603 9B2783A2 EC07A28F B5C55DF0 6F4C52C9
+      DE2BCBF6 95581718 3995497C EA956AE5 15D22618 98FA0510
+      15728E5A 8AACAA68 FFFFFFFF FFFFFFFF"""
+
 # Convert from the value supplied in the RFC to an integer
-#prime = read_hex(raw_prime)
+prime = read_hex(raw_prime_2048)
 
-q = 18617231704454832626265615696651172326566222819381257356022986674393764073508483447800645352287577092465504995486632657855407836554745463960276461621492731435221236734565312023296963
-g = 2
+#q = 18617231704454832626265615696651172326566222819381257356022986674393764073508483447800645352287577092465504995486632657855407836554745463960276461621492731435221236734565312023296963
+
+#the selected generator is 2
+generator = 2
 
 # Project TODO: write the appropriate code to perform DH key exchange
 
 def create_dh_key():
     # Creates a Diffie-Hellman key
+    #ensure that randint has sufficient entropy.
+    #If the value is == to 2**8, it is 100% predictible --> see lab2a
+    a = random.randint(1, prime - 1)
     # Returns (public, private)
-    a = random.randint(0, int(2**16))
-    return (a, pow(2, a, prime))
+    return (pow(generator, a, prime), a)
+
 
 #TODO: write function that will send a message with appropriate data
 
 def calculate_dh_secret(their_public, my_private):
     #validate public key to protect against a small subgroup attack
-    if their_public < 2 or their_public > p-1:
+    if their_public <= 1 or their_public > prime - 1:
         print("invalid public key")
         return None
+
+    # Calculate the shared secret
+    shared_secret = pow(their_public, my_private, prime)
+
+    # Calculate the shared hash -- TOM wrote code to conform to the RFC
+    # guidelines, however I could not get it working. Luke said it was
+    # beyond the scope of this stage.
+
+    # Instead, I simplified by changing the algo to SHA-1,
+    # and added a salt string (hardcoded) This is not ideal in the real world.
+    print(shared_secret)
+    shared_sec_bytes = hex(shared_secret)
+    print(shared_sec_bytes)
+    shared_hash = SHA256.new(bytes(shared_sec_bytes, "ascii"))
+    shared_hash.update(bytes("hardcodedSalt", "ascii"))
+    return shared_hash.hexdigest()
+
+    """
+    Tom, I think that this calculation is not quite right...
+    Shoudl be DIVIDE by q???  Not sure, however was not getting past
+    this condition
+
     if pow(their_public, q, prime) != 1:
         print("invalid public key")
         return None
-    # Calculate the shared secret
-    shared_secret = pow(their_public, my_private, prime)
+
 
     # Hash the value so that:
     # (a) There's no bias in the bits of the output
@@ -55,12 +97,16 @@ def calculate_dh_secret(their_public, my_private):
     #number of bits in AES-256 key
     supppubinfo = "00 00 01 00 "
     #first block of 160 bits, we need an additional 96 bits
-    KM1 = SHA.new(bytes(shared_secret + "00 00 00 01" + keyspecificinfo + supppubinfo, "ascii")).hexdigest()
-    KM2 = SHA.new(bytes(shared_sected + "00 00 00 02" + keyspecificinfo + supppubinfo, "ascii")).hexdigest()
-    KM3 = SHA.new(bytes(shared_sected + "00 00 00 03" + keyspecificinfo + supppubinfo, "ascii")).hexdigest()
+    KM1 = SHA.new(bytes(shared_secret + "00 00 00 01" + keyspecificinfo
+        + supppubinfo, "ascii")).hexdigest()
+    KM2 = SHA.new(bytes(shared_sected + "00 00 00 02" + keyspecificinfo
+        + supppubinfo, "ascii")).hexdigest()
+    KM3 = SHA.new(bytes(shared_sected + "00 00 00 03" + keyspecificinfo
+        + supppubinfo, "ascii")).hexdigest()
     #the shared hash has 60 bytes
     #we need 32 for the key and 16 more for the iv
     shared_hash = KM1 + KM2 + KM3
     return shared_hash
+    """
 
 #TODO: write funtion that will generate AES-256 key and transmit it using KEK
--- lib/comms.py
+++ lib/comms.py
@@ -1,8 +1,11 @@
 import struct
+import base64
 
 from Crypto.Cipher import XOR, AES
+from Crypto.Hash import SHA256
 
 from dh import create_dh_key, calculate_dh_secret
+
 
 class StealthConn(object):
     def __init__(self, conn, client=False, server=False, verbose=False):
@@ -12,10 +15,12 @@ class StealthConn(object):
         self.server = server
         self.verbose = verbose
         self.initiate_session()
+        self.blocksize = 32
         self.counter = 0
 
     def initiate_session(self):
         # Perform the initial connection handshake for agreeing on a shared secret
+
 
         ### TODO: Your code here!
         # This can be broken into code run just on the server or just on the client
@@ -28,15 +33,23 @@ class StealthConn(object):
             # Obtain our shared secret
             shared_hash = calculate_dh_secret(their_public_key, my_private_key)
             print("Shared hash: {}".format(shared_hash))
+        """
+        TOM -- I have changed this bit also, made it more simple. Could not get this to run.
+        If you want, we can work at strengthening it by using correct AES mode
         # key encryption key
         # AES-256, might change the mode
         self.cipher = AES.new(shared_hash[:32], AES.MODE_CFB, shared_hash[32:48])
+        """
+
+        self.cipher = AES.new(shared_hash)
+
 
         #initialise a counter to prevent replay attacks
         #use the remaining bytes from the DH handshake so this value is secret
-        self.counter = int(shared_hash[48:])
+        #self.counter = int(hex(shared_hash[48:]))
 
         #the server will generate a random key and send it via and encrypted channel
+        """ *******************    TOM CODE ********************
         if self.server:
             f = open("/dev/random", "rU")
             newkey = f.read(32)
@@ -46,14 +59,25 @@ class StealthConn(object):
         if self.client:
             newkey = self.recv()
             newiv = self.recv()
+            **************************************************"""
         #now build the content encryption cipher
         self.cipher = AES.new(newkey, AES.MODE_CFB, newiv)
 
 
     #TODO: code to append the MAC and counter
+
     def send(self, data):
         if self.cipher:
-            encrypted_data = self.cipher.encrypt(data)
+            # PEZZ - using example code from http://www.codekoala.com/posts/aes-encryption-python-using-pycrypto/
+            PADDING = '{'
+
+            # pad the text to be encrypted
+            pad = lambda s: s + (BLOCKSIZE - len(s) % BLOCKSIZE) * padding
+            encode_aes = lambda c, s: base64.b64encode(c.encrypt(pas(s)))
+            encrypted_date = encode_aes(self.cipher, data)
+
+            #encrypted_data = self.cipher.encrypt(data)
+
             if self.verbose:
                 print("Original data: {}".format(data))
                 print("Encrypted data: {}".format(repr(encrypted_data)))
@@ -65,16 +89,21 @@ class StealthConn(object):
         pkt_len = struct.pack('H', len(encrypted_data))
         self.conn.sendall(pkt_len)
         self.conn.sendall(encrypted_data)
+
+
     #TODO: code to check the MAC and counter
+
     def recv(self):
         # Decode the data's length from an unsigned two byte int ('H')
         pkt_len_packed = self.conn.recv(struct.calcsize('H'))
         unpacked_contents = struct.unpack('H', pkt_len_packed)
         pkt_len = unpacked_contents[0]
 
+        decode_aes = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
+
         encrypted_data = self.conn.recv(pkt_len)
         if self.cipher:
-            data = self.cipher.decrypt(encrypted_data)
+            data = decode_aes(self.cipher, encrypted_data)
             if self.verbose:
                 print("Receiving packet of length {}".format(pkt_len))
                 print("Encrypted data: {}".format(repr(encrypted_data)))
